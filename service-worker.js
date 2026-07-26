25fecd4d6ae9458c2c2289209ca561677d7a1a86
nuskadeberga <nuskadeberga@gmail.com>
2026-07-26 19:58:19 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,8 @@
 const CACHE = 'salivera-v1.0.0';
 const APP_SHELL = [
   './', './index.html', './style.css', './app.js', './data.js', './manifest.json',
-  './assets/icons/icon-192.png', './assets/icons/icon-512.png'
+  './assets/icons/icon-192.png', './assets/icons/icon-512.png',
+  './assets/icons/favicon.ico', './assets/icons/favicon-16.png', './assets/icons/favicon-32.png', './assets/icons/apple-touch-icon.png'
 ];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL))));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
